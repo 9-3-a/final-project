5b8802ef39e8c0139b9820e0c3cce1ec0df53e9b
Jason <haloos@me.com>
2018-09-27 19:50:48 -0400

    Start of the server-side

---
+++ config/keys/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+  'mongodb://wozu:apple112@ds157762.mlab.com:57762/jasonparedes'
+}
--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 
+
+
+
+// load keys
+const keys = require('./config/keys/keys');
+
+
+// map global promises 
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
 // PRODUCTION ONLY
  app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -14,7 +30,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-mongoose.connect('mongodb://haloos2:apple112@ds155292.mlab.com:559/2dio')
+const wineRoutes = require('./routes/wines');
+
+app.use('/wines',  wineRoutes);
+
+//mongoose.connect('mongodb://haloos2:apple112@ds155292.mlab.com:559/2dio')
 // PRODUCTION ONLY
  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
@@ -22,6 +42,8 @@ mongoose.connect('mongodb://haloos2:apple112@ds155292.mlab.com:559/2dio')
 
 // Development mode port
 const port = process.env.PORT || 5000;
-app.listen(port)
+app.listen(port,()=> {
+  console.log('server');
+});
 
 module.exports = app;
