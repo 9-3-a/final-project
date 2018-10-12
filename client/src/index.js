a464c6d96bb9ee444b4c8357ab1dadd195afa097
Amy <agrunke_3@hotmail.com>
2018-10-11 17:43:01 -0700

    Start of the navigation

--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SignInForm from './screens/login';
 import SignUpForm from './screens/register';
 import Review from './screens/review';
+import NavbarFeatures from './components/navbar';
 
-ReactDOM.render(<BrowserRouter>
+
+ReactDOM.render(
+                <BrowserRouter>
+                    <NavbarFeatures/>
                     <Switch>
                         <Route path = "/signin" component={ SignInForm } />
                         <Route path = "/signup" component={ SignUpForm } />
-                        <Route path="/review" component={Review} />
+                        <Route path= "/review" component={Review} />
                     </Switch>
                 </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
