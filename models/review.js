570400e397c062720e5a27b51ecea594bc57e27e
Jason <haloos@me.com>
2018-10-11 21:34:33 -0400

    review to wine model connection

--- models/review.js
+++ models/review.js
@@ -3,7 +3,8 @@ let mongoose = require('mongoose');
 let ReviewSchema = new mongoose.Schema({
     name: String,
     rating: Number,
-    review: String
+    review: String,
+    wine: String
 });
   
 module.exports = mongoose.model('Review', ReviewSchema);
